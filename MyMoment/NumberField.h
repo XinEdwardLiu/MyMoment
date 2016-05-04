014c4939b0da53d8ef1b93fb02525d138b15d28b
Edward Liu <xinedwardliu@gmail.com>
2016-05-04 01:07:17 -0400

    注册验证、输入11位数字可正常使用
    rt

---
+++ MyMoment/NumberField.h
@@ -0,0 +1,15 @@
+//
+//  NumberField.h
+//  MyMoment
+//
+//  Created by Edward Liu on 16/5/3.
+//  Copyright © 2016年 Edward Liu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NumberField : NSTextField
+
+-(void) textDidEndEditing:(NSNotification *)aNotification;
+
+@end
---
+++ MyMoment/NumberField.m
@@ -0,0 +1,26 @@
+//
+//  NumberField.m
+//  MyMoment
+//
+//  Created by Edward Liu on 16/5/3.
+//  Copyright © 2016年 Edward Liu. All rights reserved.
+//
+
+#import "NumberField.h"
+
+@implementation NumberField
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+-(void) textDidEndEditing:(NSNotification *)aNotification {
+    // replace content with its intValue ( or process the input's value differently )
+    [self setIntValue:[self intValue]];
+    // make sure the notification is sent back to any delegate
+   [super controlTextDidEndEditing:aNotification];
+}
+
+@end
